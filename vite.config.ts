d0754f5ecfd03cb8a291abb08ff0fe9f57e11862
santiagoaluas <82826216+santiagoaluas@users.noreply.github.com>
2025-08-13 02:03:03 -0500

    Para produccion

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { fileURLToPath } from 'node:url' // Usa 'node:url' en lugar de 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@", replacement: resolve(__dirname, "./src") },
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url))
+      },
       {
         find: /^@mui\/icons-material\/esm\/(.*)\.js$/,
         replacement: '@mui/icons-material/$1'
