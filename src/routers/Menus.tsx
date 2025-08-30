aa8f807486564179b0b0e510c5d85285ef29d6b4
santiagoaluas <82826216+santiagoaluas@users.noreply.github.com>
2025-08-30 01:06:57 -0500

    Cambios nuevos

--- src/routers/Menus.tsx
+++ src/routers/Menus.tsx
@@ -37,7 +37,7 @@ const Menus: MenuItem[] = [
   },
   {
     name: "Migraciones",
-    roles: ["admin", "user"],
+    roles: ["admin"],
     icon: WhiteIcon(ChecklistRtlIcon),
     children: [
       {
