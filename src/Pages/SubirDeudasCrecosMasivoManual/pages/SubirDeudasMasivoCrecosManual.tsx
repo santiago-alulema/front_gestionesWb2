018f8c040c699a215d8360fc258df215c2b1c63f
santiagoaluas <82826216+santiagoaluas@users.noreply.github.com>
2026-03-11 06:37:24 -0500

    subida manual crecos

--- src/Pages/SubirDeudasCrecosMasivoManual/pages/SubirDeudasMasivoCrecosManual.tsx
+++ src/Pages/SubirDeudasCrecosMasivoManual/pages/SubirDeudasMasivoCrecosManual.tsx
@@ -1,11 +1,26 @@
+import { useLoading } from "@/components/LoadingContext";
 import UploadExcel from "@/components/UploadExcel"
 import { listarUsuariosServiceWeb } from "@/Pages/AdministracionUsuario/services/ServiciosWebCrudUsuario"
 import { SubirDeudasCrecosManualSW } from "@/Pages/SubirDeudasCrecosMasivoManual/services/SubirDeudasCrecosServiciosWeb";
+import { showAlert } from "@/utils/modalAlerts";
 
 const SubirDeudasMasivoCrecosManual = () => {
 
+    const { startLoading, stopLoading } = useLoading();
     const subirDeudasC = async (list: any[]) => {
-        await SubirDeudasCrecosManualSW(list);
+        try {
+            startLoading()
+            await SubirDeudasCrecosManualSW(list);
+            const configAlert = {
+                title: "Correcto",
+                message: "Se actualizo correctamente",
+                type: 'success',
+                callBackFunction: false
+            };
+            showAlert(configAlert);
+        } finally {
+            stopLoading();
+        }
     }
     return (
         <UploadExcel
